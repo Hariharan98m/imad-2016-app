a218c6afbc86c104cc4b7212629b94bb7c94ef24
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-09-30 13:08:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'hari.jpg'));
 });
 
 app.get('/ui/style.css', function (req, res) {
