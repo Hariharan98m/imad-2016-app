8180f56c10597ed9e0fdf6359a9979dfd9bcbc68
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-09-30 13:04:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 
+app.get('http://hariharan98m.imad.hasura-app.io/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
