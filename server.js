7d4526b09ec6f5124ac7421f936fd14f47aa3b59
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-10-10 18:06:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
     res.send(f(articles.articleName));
 });
 
