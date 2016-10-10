aaa489fb63ec0e0a16c973d50de926185e7a384c
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-10-10 21:20:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,15 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-var img=document.getElementById('image');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-        var interval=setInterval(moveRight(),50);
-};
+var counter=0;
+var button=document.getElementById('counter');
 
+
+
+
+
+
+//Render the variable in correct form
+button.onclick=function(){
+  counter+=1;
+  var span=document.getElementById('count');
+  span.innerHTML=counter;
+  
+};
