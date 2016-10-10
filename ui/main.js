b9d8db1ba6738f08984beb7d6a1d30c39682b19f
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-10-10 19:21:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ element.innerHTML='New Value';
 var img=document.getElementById('image');
 var marginLeft=0;
 function moveRight(){
-    marginLeft+=10
+    marginLeft+=10;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-        var interval=setiinterval(moveRight(),100)
-}
+        var interval=setInterval(moveRight(),100);
+};
 
