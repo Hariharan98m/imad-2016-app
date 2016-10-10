6a33079b11e235acc5766832858de6be59bf8090
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-10-10 19:53:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/:articleName',function(req,res){
 
 var counter=0;
 app.get('/counter',function(req, res){
-   res.send(counter);
+   res.send(counter.toString());
 });
 
 app.get('/ui/madi.png', function (req, res) {
