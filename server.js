f1b010d27c8622fc36a368f431c91ac973a8f6cc
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-10-11 20:13:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,13 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names=[];
+app.get('/submitbtn/:name', function (req, res) {
+  var name=req.params.name;
+  names.push(name);
+  //JSON
+  res.send(JSON.stringify(names));
+});
 var counter=0;
 
 app.get('/counter', function (req, res) {
