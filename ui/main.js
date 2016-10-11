617d7b7bcff84ac3379730e69c1ce8d4fed6e752
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-10-11 20:06:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,3 +30,22 @@ request.send(null);
   span.innerHTML=counter;
   
 };*/
+
+//Submit name
+var nameInput= document.getElementById('name');
+var Name=nameInput.value;
+var submit=document.getElementById('submitbtn');
+submit.onclick=function(){
+    //Make a request to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+        
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+    
+};
