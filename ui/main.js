e0663d79e707ede73d246607fce93f479ef53a51
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-10-11 19:21:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,27 @@
 var counter=0;
 var button=document.getElementById('counter');
+//
+//Make a request to the counter endpoint
+    var request=new XMLHttpRequest();
+//Capture the request and store it in a variable
+    request.onreadystatechange=function(){
+      if (request.readyState==XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status==200){
+              var counter=request.responseText;
+          }
+      }  
+      //Not done yet
+    };
 
 
-
-
-
-
+//Make the request
+request.open('GET','http://hariharan98m.imad.hasura-app.io', true);
+request.send(null);
 //Render the variable in correct form
-button.onclick=function(){
+/*button.onclick=function(){
   counter+=1;
   var span=document.getElementById('count');
   span.innerHTML=counter;
   
-};
+};*/
