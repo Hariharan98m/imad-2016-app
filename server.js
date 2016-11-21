ad0af1310822a3f83435b51452d6b41be9f500f4
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-21 13:32:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,11 +113,15 @@ app.get('/submitbtn', function (req, res) {
   var password=req.query.password;
   pool.query("SELECT * from users where name='"+name+"' and password='"+name+"'",function(err,result){
         if(err){
-            res.status(500).send('Invalid username/password');
+            res.status(500).send(JSON.stringify(err));
         }
         else
-        {
+        {   if(result.rows.length===0){
+            res.send('Invalid username/password');
+            }
+            else{
             res.send('Successful check for credentials');
+            }
         }
     });
     //respond with data
