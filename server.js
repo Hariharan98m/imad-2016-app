ff1b6287bd626550112b5bff40ba0d091ac953d1
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-21 13:11:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/test-db',function(req,res){
         }
         else
         {
-            res.send(result.rows[0]);
+            res.send(result.rows[0].value);
         }
     });
     //respond with data
