ccb171f7ed668f61dd06824c54e432a2353a5161
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-23 21:20:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.get('/test-db',function(req,res){
 
 function hash(input){
     var hashed=crypto.pbkdf2Sync(input,'this-is-a-random-string',10000,512,'sha512');
-    return hashed.toString(hex);
+    return hashed.toString('hex');
 }
 
 app.get('/hash/:input',function(req,res){
