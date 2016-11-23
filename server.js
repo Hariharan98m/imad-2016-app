1a06059b69a7ba7dcf974f185533bb0c68db23dc
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-23 22:12:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ function temp(data){
     <meta name='viewport' content='width=device-width, initial-scale=1'/>
     </head>
     <body>
-        <div class=special>
+        <div class=>
             <div>
                 <a href='/'>Home</a>
             </div>
