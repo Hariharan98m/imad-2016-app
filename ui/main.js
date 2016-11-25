b7321e5a3d48e5dfcf61a51bdd586dd9cf880c1a
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-25 19:19:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -71,6 +71,91 @@ request.send(null);
   
 //};
 
+
+
+//new user
+var sub=document.getElementById('signsubmitbtn');
+submit.onclick=function(){
+    
+    //Make a request to the server and send the name
+    var request=new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+      if (request.readyState==XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status==200){
+              //Capture the response and save it
+              console.log('Logged in');
+              alert('Logged in');
+              var reply=request.responseText;
+              var cont=document.getElementById('continue');
+              if(reply=='User successfully created'){
+                  cont.innerHTML='Continue';
+              }
+              else{
+                  cont.innerHTML='';
+              }
+              var message=document.getElementById('message');
+              message.innerHTML=reply;
+              
+          }
+          else if(status==403){
+              console.log('Password is incorrect');
+                alert('Password is incorrect');
+              
+          }
+              else{
+                  alert('Something went wrong');
+              }
+          
+      }  
+      //Not done yet
+    };
+    
+
+var name= document.getElementById('name').value;
+var password=document.getElementById('pw').value;
+var pswd=PasswordInput.value;
+request.open('POST','http://hariharan98m.imad.hasura-app.io/signin', true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username:name},{password:password}));
+}; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Submit name
 var submit=document.getElementById('subbtn');
 submit.onclick=function(){
