0b9dc3ca9e7f607385e5a90b0b1ed07da31f11ad
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-26 08:54:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -198,29 +198,7 @@ function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ['pbkdf2Sync','10000',salt,hashed.toString('hex')].join('$');
 }
-
-app.get('/hash/:input',function(req,res){
-    //make a request
-    var hashedString=hash(req.params.input,'this-is-a-random-string');
-    res.send(hashedString);
-    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.post("/create-user", function (req, res) {
+app.post('/create-user', function (req, res) {
     //username,password
     //JSON
     var username=req.body.username;
