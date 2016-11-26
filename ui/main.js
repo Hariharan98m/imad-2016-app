919514c0b7b57290e64289a379fb93dff8df279f
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-26 10:21:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -100,7 +100,7 @@ sub.onclick=function(){
               
           }
         else{
-              alert('Something went wrong');
+              alert('Username already taken. Choose a different one');
             }
           
       }  
@@ -109,13 +109,53 @@ sub.onclick=function(){
     
 
 var username= document.getElementById('name').value;
-alert(username.toString());
 var password=document.getElementById('pw').value;
 request.open('POST','http://hariharan98m.imad.hasura-app.io/create-user', true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({'username':username,'password':password}));
 }; 
 
+
+//existing user
+var sub1=document.getElementById('loginsubmitbtn');
+sub1.onclick=function(){
+    
+    //Make a request to the server and send the name
+    var request=new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+      if (request.readyState==XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status==200){
+              //Capture the response and save it
+              var reply=request.responseText;
+              var cont=document.getElementById('continue');
+              if(reply=='Successful check for credentials'){
+                  cont.innerHTML='Continue';
+              }
+              else{
+                  cont.innerHTML='';
+              }
+              var message=document.getElementById('message');
+              message.innerHTML=reply;
+              
+          }
+        else{
+              alert(request.responseText);
+            }
+          
+      }  
+      //Not done yet
+    };
+    
+
+var username= document.getElementById('name').value;
+var password=document.getElementById('pw').value;
+request.open('POST','http://hariharan98m.imad.hasura-app.io/create-user', true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({'username':username,'password':password}));
+}; 
 
 
 
