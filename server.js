15ad783407c4cf4220e9f8cd9d18ef871d4cba95
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-28 19:13:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -297,7 +297,7 @@ app.post('/comment',function(req,res){
         console.log('cookie set');
         pool.query("Select name from users where id='"+req.session.auth.userId.toString()+"'",function(err,result){
         pool.query("update articles set comments=$1 where title=$2",[user+': '+comment+'\n',title],function(err,result){
-        pool.query("SELECT * from articles where title=$1",[title]function(err,result){
+        pool.query("SELECT * from articles where title=$1",[title],function(err,result){
             console.log(result.rows);
             res.send(result.rows[0].comments);
         });
