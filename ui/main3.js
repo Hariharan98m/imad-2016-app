cfd872f1db3b2fb2e9bee870254e0ddac481e785
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-28 18:10:49 +0530

    [imad-console] Updates ui/main3.js

--- ui/main3.js
+++ ui/main3.js
@@ -5,25 +5,31 @@
         // Create a request object
         
         var request = new XMLHttpRequest();
-        /*
+        
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
                 // Take some action
                 if (request.status === 200) {
                     // clear the form & reload all the comments
-                    document.getElementById('comment_text').value = '';
-                    loadComments();    
-                } else {
-                    alert('Error! Could not submit comment');
-                }
+                    var reply=request.responseText;
+                    if (reply==='Log in to comment'){
+                        alert(reply);
+                    }
+                    else{
+                    var cont=document.getElementById('comment_form');
+                    cont.innerHTML=request.responseText;
+                    }   
+                } 
                 submit.value = 'Submit';
           }
         };
-        */
+        
         // Make the request
         var comment = document.getElementById('commentbox').value;
-        var text = document.getElementById('tit').innerHTML;
-        console.log(comment);
-        console.log(text);
+        var title = document.getElementById('tit').innerHTML;
+        request.open('POST', '/comment', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({comment: comment,title:title}));  
+        submit.value = 'Submitting...';
     };
