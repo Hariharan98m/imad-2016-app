da582107e964b499d894a4b3f32ce298da57fe47
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-29 07:58:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -287,7 +287,7 @@ app.get('/logout',function(req,res){
 app.post('/comment',function(req,res){
     var comment=req.body.comment.toString();
     var title=req.body.title.toString();
-    title.strip();
+    title=title.trim();
     console.log('I ve striped title:'+title);
     console.log('I m here in the comments page'+comment+' '+title);
     if (req.session&&req.session.auth&&req.session.auth.userId){
