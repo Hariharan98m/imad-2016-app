3779cb3501b14ef1966e8b3e1b1ebdaf065aab6d
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-29 20:14:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -109,9 +109,11 @@ sub.onclick=function(){
               reply="<div style='background-color:"+clas+"';>"+reply+'</div>';
               var message=document.getElementById('message');
               message.innerHTML=reply;
+              sub.value='Submit';
           }
         else{
               alert(request.responseText);
+              sub.value='Submit';
             }
       }  
       //Not done yet
@@ -121,6 +123,8 @@ var password=document.getElementById('pw').value;
 request.open('POST','http://hariharan98m.imad.hasura-app.io/login', true);
 request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({'username':username,'password':password}));
+sub.value='Submitting...';
+    
 };
 
 
