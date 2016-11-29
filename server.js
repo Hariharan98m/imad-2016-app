8ddb43fa748b381618bf0217ab74493888ee390f
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2016-11-29 08:22:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -298,7 +298,7 @@ app.post('/comment',function(req,res){
                     user=result.rows[0].name.toString();
                     console.log(user);
                 });
-                pool.query("select * from articles where title=$1",[title],function(err,result){
+                pool.query("select comments from articles where title=$1",[title],function(err,result){
                     his=result.rows[0].comments.toString();
                     console.log('this is history:'+his+'');
                 });
